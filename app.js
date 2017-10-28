bc69356fd6a993c4ccbec229a462c0ba013f4010
jskonst <jskonst@yandex.ru>
2017-10-28 11:07:07 +0300

    added https sample and self signed serts

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ app.use(express.static(__dirname + "/clientApp"));
 // Загружаем модуль файловой системы
 var fs = require('fs');
 
+var credentials = {
+    key: fs.readFileSync('./tmpSert/key.pem'),
+    cert: fs.readFileSync('./tmpSert/certificate.pem')
+  };
 
 const http = require('http');
+const https = require('https');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3000; //80
+const ports = 3003; //443
 
-app.listen(port);
+// app.listen(port);
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(port);
+httpsServer.listen(ports);
 
 // app.get("/", function(request, response){
   
